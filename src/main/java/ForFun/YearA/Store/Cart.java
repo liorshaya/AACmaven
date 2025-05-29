37735f858fedaa14abe1c66127fa2c3e4d03eeb8
liorshaya <84973877+liorshaya@users.noreply.github.com>
2025-05-29 21:13:16 +0300

    regular

---
+++ src/main/java/ForFun/YearA/Store/Cart.java
@@ -0,0 +1,12 @@
+package ForFun.YearA.Store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private List<Product> products;
+
+    public Cart(){
+        this.products = new ArrayList<>();
+    }
+}
