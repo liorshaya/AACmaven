7e1bdd069e5dc252b02b28eda5ae0924ff894d51
Lior Shaya <lior160500@gmail.com>
2025-06-25 23:10:52 +0300

    regular

--- src/main/java/YearA/TermB/OOP1/lesson11/exe1/TimeBoundList.java
+++ src/main/java/YearA/TermB/OOP1/lesson11/exe1/TimeBoundList.java
@@ -4,7 +4,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TimeBoundList<E>{
-    private long timeoutMillis;
-    private List<TimedElement<E>> elements = new ArrayList<>();
-    private List<E> removedElements = new ArrayList<>();
+    private int timeoutSec;
+    private List<TimedElement<E>> elements;
+    private List<E> removedElements;
+
+    public TimeBoundList(int timeoutSec){
+        this.elements = new ArrayList<>();
+        this.removedElements = new ArrayList<>();
+        this.timeoutSec = timeoutSec;
+    }
+
+    public void removeOld(){
+        new Thread(() -> {
+            while (true) {
+                for(int i = 0 ; i< elements.size() ; i++ ){
+                    TimedElement<E> te = elements.get(i);
+                    long duration = System.currentTimeMillis() - te.getInsertTime();
+                    if(duration >= timeoutSec * 1000L){
+                        removedElements.add(te.getElement());
+                        elements.remove(i);
+                        System.out.println("The element " + i + " has removed!");
+                        i--;
+                    }
+                }
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+    }
+
+    public List<E> getRemovedElements(){
+        return this.removedElements;
+    }
+
+    public void refresh(int index){
+        if(index >= 0 && index < elements.size()){
+            elements.get(index).refrashTime();
+        }
+    }
+
+
 }
